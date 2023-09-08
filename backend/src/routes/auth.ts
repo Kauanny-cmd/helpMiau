79618f236816ebc618d6af0d62cdcd5d10bc7936
kauanny-cmd <kauannyvieira8@gmail.com>
2023-09-08 17:20:35 -0300

    #2 Edição de usuário

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -28,4 +28,33 @@ export async function authRoutes(app: FastifyInstance){
       reply.status(400).send({ error: 'Invalid data' });
     }
   })
+
+  app.put('/usuario/:id', async (req, reply) => {
+    try {
+      const paramsSchema = z.object({
+        id: z.string().uuid(),
+      })
+      const { id } = paramsSchema.parse(req.params)
+
+      const usuarioSchema = z.object({
+        nome: z.string(),
+        login: z.string(),
+        avatarUrl: z.string(),
+        telefone: z.string(),
+        pets: z.array(z.string()),
+      });
+
+      const usuarioData = usuarioSchema.parse(req.body);
+
+      const updatedUsuario = await prisma.usuario.update({
+        where: { id },
+        data: usuarioData,
+      });
+
+      reply.code(200).send(updatedUsuario);
+    } catch (error) {
+      console.error(error);
+      reply.code(500).send({ error: 'Erro ao atualizar usuário.' });
+    }
+  });
 }

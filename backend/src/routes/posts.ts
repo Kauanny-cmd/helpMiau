ff78b9d060206672354e2b2e30b7c68c0b05905d
kauanny-cmd <kauannyvieira8@gmail.com>
2023-09-13 14:58:26 -0300

    #6  Criação de novos posts #8 criação do cadastro dos animais

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
+import {z} from 'zod'
 
 export async function postsRoutes(app: FastifyInstance) {
   app.get('/posts', async (req, res) => {
@@ -20,5 +21,30 @@ export async function postsRoutes(app: FastifyInstance) {
         excerpt: post.descricao.substring(0, 115).concat('...'), 
       }
     }))
+  })
+
+  app.post('/createPost', async (request) => {
+    const bodySchema = z.object({
+      descricao: z.string(),
+      imagens: z.string().array(),
+      isPublic: z.coerce.boolean().default(false),
+      localizacao: z.coerce.string().array(),
+      filtros: z.string().array(),
+      userId: z.string()
+    })
+
+    const postData = bodySchema.parse(request.body)
+    const post = await prisma.post.create({
+      data: {
+        descricao: postData.descricao,
+        imagens: postData.imagens,
+        isPublic: postData.isPublic,
+        localizacao: postData.localizacao,
+        filtros: postData.filtros,
+        userId: postData.userId,
+      },
+    })
+
+    return post
   })
 }

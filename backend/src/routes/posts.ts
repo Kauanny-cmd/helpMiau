3afc88efa9f6571bae7ccdf046370c9eca5e5fff
kauanny-cmd <kauannyvieira8@gmail.com>
2023-09-13 15:30:43 -0300

    #7 Exclusão do post #9 exclui o cadastro dos pets

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -3,7 +3,7 @@ import { prisma } from '../lib/prisma'
 import {z} from 'zod'
 
 export async function postsRoutes(app: FastifyInstance) {
-  app.get('/posts', async (req, res) => {
+  app.get('/posts', async () => {
     const posts = await prisma.post.findMany({
       orderBy:{
         createdAt: 'asc'
@@ -34,6 +34,7 @@ export async function postsRoutes(app: FastifyInstance) {
     })
 
     const postData = bodySchema.parse(request.body)
+    // Cria um novo post seguindo o esquema dado
     const post = await prisma.post.create({
       data: {
         descricao: postData.descricao,
@@ -46,5 +47,51 @@ export async function postsRoutes(app: FastifyInstance) {
     })
 
     return post
+  })
+
+  app.delete('/deletePost/:id', async (request, reply) => {
+    try {
+      const bodySchema = z.object({
+        postId: z.string(),
+        userId: z.string()
+      })
+  
+      const postData = bodySchema.parse(request.body)
+
+      // Verifica se o ID do usuário está presente na solicitação
+      if (!postData.userId) {
+        reply.code(401).send({ error: 'Usuário não autenticado.' });
+        return;
+      }
+      // Busca as informações do post, incluindo o ID do usuário que criou o post
+      const post = await prisma.post.findUnique({
+        where: {
+          id: postData.postId,
+        },
+        select: {
+          userId: true,
+        },
+      });
+      // Verifica se o post existe
+      if (!post) {
+        reply.code(404).send({ error: 'Post não encontrado.' });
+        return;
+      }
+      // Verifica se o post pertence ao usuário que está tentando excluí-lo
+      if (post.userId !== postData.userId) {
+        reply.code(403).send({ error: 'Você não tem permissão para excluir este post.' });
+        return;
+      }
+      // Exclui o post se tudo estiver correto
+      await prisma.post.delete({
+        where: {
+          id: postData.postId,
+        },
+      });
+      reply.send({ message: 'Post excluído com sucesso.' });
+    } catch (e) {
+      console.error(e);
+      reply.code(500).send({ error: 'Erro ao excluir postagem.' });
+    }
   })
 }

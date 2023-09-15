279ec2c8cfd39e9c8ca9ed852aa2a58dcc059b96
kauanny-cmd <kauannyvieira8@gmail.com>
2023-09-15 11:22:38 -0300

    #12 Cadastro de tópico de conversa

--- backend/src/routes/topic.ts
+++ backend/src/routes/topic.ts
@@ -19,4 +19,24 @@ export async function topicsRoutes(app: FastifyInstance) {
       }
     }))
   })
+
+  app.post('/createTopic', async (request) => {
+    const bodySchema = z.object({
+      titulo: z.string(),
+      descricao: z.string(),
+      userId: z.string()
+    })
+
+    const topicData = bodySchema.parse(request.body)
+    // Cria um novo tópico seguindo o esquema dado
+    const topico = await prisma.topico.create({
+      data: {
+        titulo: topicData.titulo,
+        descricao: topicData.descricao,
+        userId: topicData.userId,
+      },
+    })
+
+    return topico
+  })
 }

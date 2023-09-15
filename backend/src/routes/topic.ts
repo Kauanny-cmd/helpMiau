5d2297a6b6b4206a7565fdcf0540dddca3c71b2c
kauanny-cmd <kauannyvieira8@gmail.com>
2023-09-15 11:57:00 -0300

    #13 exclusão do tópico

--- backend/src/routes/topic.ts
+++ backend/src/routes/topic.ts
@@ -39,4 +39,50 @@ export async function topicsRoutes(app: FastifyInstance) {
 
     return topico
   })
+
+  app.delete('/deleteTopic/:id', async (request, reply) => {
+    try {
+      const bodySchema = z.object({
+        topicId: z.string(),
+        userId: z.string()
+      })
+  
+      const topicData = bodySchema.parse(request.body)
+
+      // Verifica se o ID do usuário está presente na solicitação
+      if (!topicData.userId) {
+        reply.code(401).send({ error: 'Usuário não autenticado.' });
+        return;
+      }
+      // Busca as informações do tópico, incluindo o ID do usuário que criou o tópico
+      const topico = await prisma.topico.findUnique({
+        where: {
+          id: topicData.topicId,
+        },
+        select: {
+          userId: true,
+        },
+      });
+      // Verifica se o tópico existe
+      if (!topico) {
+        reply.code(404).send({ error: 'Tópico não encontrado.' });
+        return;
+      }
+      // Verifica se o tópico pertence ao usuário que está tentando excluí-lo
+      if (topico.userId !== topicData.userId) {
+        reply.code(403).send({ error: 'Você não tem permissão para excluir este tópico.' });
+        return;
+      }
+      // Exclui o tópico se tudo estiver correto
+      await prisma.topico.delete({
+        where: {
+          id: topicData.topicId,
+        },
+      });
+      reply.send({ message: 'Tópico excluído com sucesso.' });
+    } catch (e) {
+      console.error(e);
+      reply.code(500).send({ error: 'Erro ao excluir o tópico de conversa.' });
+    }
+  })
 }

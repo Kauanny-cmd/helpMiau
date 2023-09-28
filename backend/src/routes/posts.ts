9a22f46627ae662f1efeddb07a7231995ec25e95
kauanny-cmd <kauannyvieira8@gmail.com>
2023-09-28 15:56:58 -0300

    #8 Adição de comentários nos posts

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -1,14 +1,22 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
-import {z} from 'zod'
+import { z } from 'zod'
 
 export async function postsRoutes(app: FastifyInstance) {
   app.get('/posts', async () => {
     const posts = await prisma.post.findMany({
-      orderBy:{
+      orderBy: {
         createdAt: 'asc'
+      },
+      include:{
+        comentarios:{
+          orderBy:{
+            createdAt: 'asc'
+          }
+        }
       }
     })
+    
     return (posts.map((post) => {
       return {
         id: post.id,
@@ -19,7 +27,14 @@ export async function postsRoutes(app: FastifyInstance) {
         avisos: post.avisos,
         isPublic: post.isPublic,
         usuario: post.userId,
-        excerpt: post.descricao.substring(0, 115).concat('...'), 
+        excerpt: post.descricao.substring(0, 115).concat('...'),
+        comentarios: post.comentarios.map((comment) => {
+          return {
+            id: comment.id,
+            descricao: comment.descricao,
+            userId: comment.userId,
+          };
+        }),
       }
     }))
   })
@@ -52,13 +67,52 @@ export async function postsRoutes(app: FastifyInstance) {
     return post
   })
 
+  app.post('/commentPost/:id', async (request, reply) => {
+    try {
+      const bodySchema = z.object({
+        descricao: z.string(),
+        userId: z.string(),
+        postId: z.string(),
+      })
+
+      const postCommentData = bodySchema.parse(request.body)
+
+      // Verificando se o post existe
+      const post = await prisma.post.findUnique({
+        where: {
+          id: postCommentData.postId,
+        }
+      })
+
+      if (!post) {
+        reply.code(404).send({ error: 'Post não existe!' })
+        return
+      }
+
+      // Criando comentário
+      const commentPost = await prisma.comentarioPost.create({
+        data: {
+          descricao: postCommentData.descricao,
+          userId: postCommentData.userId,
+          postId: postCommentData.postId
+        }
+      })
+
+      reply.send({ message: 'Comentário adicionado com sucesso.', commentPost });
+      return commentPost
+    } catch (e) {
+      console.log(e)
+      reply.code(500).send({ error: 'Erro ao comentar na postagem.' });
+    }
+  })
+
   app.delete('/deletePost/:id', async (request, reply) => {
     try {
       const bodySchema = z.object({
         postId: z.string(),
         userId: z.string()
       })
-  
+
       const postData = bodySchema.parse(request.body)
 
       // Verifica se o ID do usuário está presente na solicitação

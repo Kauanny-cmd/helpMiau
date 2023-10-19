9a0e95b7afabe3b1d4a0c90aa2a9c3e4ce40dd53
kauanny-cmd <kauannyvieira8@gmail.com>
2023-10-19 14:54:04 -0300

    estilo global

---
+++ mobile/src/global/style.tsx
@@ -0,0 +1,9 @@
+export default {
+  whiteColor: '#F9F9FB',
+  backgroundColor: '#FAFAFA',
+  primaryColor: '#588E95',
+  secondaryColor: '#ED6401',
+  darkColor: '#121214',
+  dangerColor: '#FF5447',
+  grayText: 'rgba(151, 151, 151, 0.8)',
+}

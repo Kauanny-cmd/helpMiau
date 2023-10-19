750fd207931d39e0f6bbcf97ebcd47bb611e1644
kauanny-cmd <kauannyvieira8@gmail.com>
2023-10-19 14:58:15 -0300

    #18 rota main

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+
+import AuthNavigator from './src/routes/authNavitagor';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthNavigator />
+      <StatusBar translucent/>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

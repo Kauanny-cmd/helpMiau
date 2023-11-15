19be33d558c113f429fd972641105ab19ad91939
kauanny-cmd <kauannyvieira8@gmail.com>
2023-11-15 10:21:49 -0300

    #3 mudança na autenticação do usuário

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,82 +1,48 @@
 import { FastifyInstance } from 'fastify';
-import { createClient } from '@supabase/supabase-js';
 import { prisma } from '../lib/prisma';
 import { z } from 'zod';
-import axios from 'axios';
+import * as jwt from 'jsonwebtoken';
 
 export async function authRoutes(app: FastifyInstance) {
-  const clientId = process.env.CLIENT_ID;
-  const clientSecret = process.env.CLIENT_SECRET; // Substitua pelo seu segredo do cliente Google
-  const redirectUri = process.env.REDIRECT_URI;
-  /*const RESPONSE_TYPE = 'code';
-  const SCOPE = encodeURI('openid email');*/
-  const SUPABASE_URL = 'https://vxqklgduvcftxkloprpd.supabase.co';
-  const SUPABASE_KEY = process.env.SUPABASE_KEY as string;
-  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY,{
-    auth:{
-      persistSession: false,
-    }
-  });
-  
+
   app.post('/register', async (req, reply) => {
     const bodySchema = z.object({
-      code: z.string(),
+      nome: z.string(),
+      login: z.string().email(),
+      senha: z.string(),
     });
-    const { code } = bodySchema.parse(req.body);
 
     try {
-      const tokenResponse = await axios.post(
-        'https://oauth2.googleapis.com/token',
-        {
-          code,
-          client_id: clientId,
-          client_secret: clientSecret,
-          redirect_uri: redirectUri,
-          grant_type: 'authorization_code',
-        }
-      );
+      const { nome, login, senha } = bodySchema.parse(req.body);
 
-      // Verifique se a resposta contém tokens
-      const { access_token, id_token } = tokenResponse.data;
-
-      //Autenticar no Supabase
-      const supabaseResponse = await supabase.auth.signInWithIdToken({
-        provider: 'google',
-        token: id_token,
-        access_token,
-      });
-
-      // Validação com zod
-      const userSchema = z.object({
-        id: z.string(),
-        nome: z.string(),
-        login: z.string(),
-        fotoUrl: z.string(),
-      });
-
-      const userData = userSchema.parse(supabaseResponse.data);
-
-      let user = await prisma.usuario.findUnique({
+      // Verificar se o usuário já existe no banco de dados
+      const existingUser = await prisma.usuario.findUnique({
         where: {
-          id: userData.id,
-        },
+          login,
+        }
       });
 
-      if (!user) {
-        user = await prisma.usuario.create({
-          data: {
-            nome: userData.nome,
-            login: userData.login,
-            avatarUrl: userData.fotoUrl,
-            telefone: "",
-            pets: [],
-          },
-        });
+      if (existingUser) {
+        return reply.status(400).send({ error: 'Usuário já registrado com este e-mail' });
       }
 
-      //console.log('UserData' + userData);
-      return reply.send({ message: 'Usuário registrado com sucesso', user });
+      // Criar usuário no banco de dados
+      const user = await prisma.usuario.create({
+        data: {
+          nome,
+          login,
+          telefone: "",
+          avatarUrl: "",
+        }
+      });
+
+      const token = jwt.sign({ userId: user.id, userEmail: user.login }, 'secreto', {
+        expiresIn: '7d',
+      });
+
+      return reply.send({ message: 'Conta criada com sucesso', user, token });
     } catch (e) {
+      console.error('Erro:', e);
       return reply.status(400).send({ error: 'Erro durante o registro' });
     }
   });

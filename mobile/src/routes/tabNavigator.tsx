89ab1b2975428ed0b5a66ed0a12db5099ccce8f2
kauanny-cmd <kauannyvieira8@gmail.com>
2023-11-15 10:29:34 -0300

    #18 esboço da tabnavigation

---
+++ mobile/src/routes/tabNavigator.tsx
@@ -0,0 +1,37 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Colors from '../global/style';
+
+import Home from '../screens/Home';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = '';
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'paw'
+              : 'paw-outline';
+          } else if (route.name === 'Post') {
+            iconName = focused ? 'add-circle-sharp' : 'add-circle-outline';
+          } else if (route.name === 'Chat') {
+            iconName = focused ? 'chatbubbles-sharp' : 'chatbubbles-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: `${Colors.secondaryColor}`,
+        tabBarInactiveTintColor: `${Colors.primaryColor}`,
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
+  );
+}
+export default TabNavigation

9fa54346508ea7bb65fa41d2601ec73215a5fcd7
kauanny-cmd <kauannyvieira8@gmail.com>
2023-11-15 10:23:20 -0300

    #17 botão

---
+++ mobile/src/components/Button/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { GestureResponderEvent, Text, TouchableOpacity } from "react-native";
+
+import { Icon } from "@rneui/base";
+
+import Colors from '../../global/style';
+import styles from "./style";
+
+type ButtonProps = {
+  colorButton?: string;
+  colorText?: string;
+  colorBorder?: string;
+  title: string;
+  link?: boolean;
+  onPress?: ((event: GestureResponderEvent) => void) | undefined
+}
+
+const Button = ({
+  colorButton,
+  colorText,
+  colorBorder,
+  title,
+  link,
+  onPress,
+  ...rest
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={{
+        ...styles.container,
+        backgroundColor: link ? "transparent" : colorButton,
+        borderColor: colorBorder,
+      }}
+      {...rest}
+      onPress={onPress} 
+    >
+      <Text
+        style={{
+          ...styles.titleButton,
+          color: link ? Colors.primaryColor : colorText,
+        }}
+      >
+        {title}
+      </Text>
+      {link ? (
+        <Icon
+          name="arrow-forward"
+          type="material"
+          size={18}
+          color={Colors.whiteColor}
+        />
+      ) : null}
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
---
+++ mobile/src/components/Button/style.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    marginBottom: 23,
+    borderWidth: 1,
+    borderRadius: 5
+  },
+  titleButton: {
+    fontSize: 16,
+    fontWeight: "bold",
+    paddingHorizontal: 4,
+    lineHeight: 18
+  },
+});
+
+export default style;

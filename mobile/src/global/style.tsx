11cbe469903c67fdb05be7e6da49ee00929552db
kauanny-cmd <kauannyvieira8@gmail.com>
2023-12-07 14:52:40 -0300

    #17 colores globais

--- mobile/src/global/style.tsx
+++ mobile/src/global/style.tsx
@@ -5,5 +5,7 @@ export default {
   secondaryColor: '#ED6401',
   darkColor: '#121214',
   dangerColor: '#FF5447',
+  textDangerColor: '#FFCCC8',
   grayText: 'rgba(151, 151, 151, 0.8)',
+  grayTextColor4: '#BDBDBD',
 }

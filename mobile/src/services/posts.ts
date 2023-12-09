ec57541fd872aa2253d31ef271ea6ab48179e99e
kauanny-cmd <kauannyvieira8@gmail.com>
2023-12-09 18:59:54 -0300

    #20 add post e get de comentarios

--- mobile/src/services/posts.ts
+++ mobile/src/services/posts.ts
@@ -1,4 +1,4 @@
-import { IPost } from "src/types/IPost";
+import { IPost, IPostCommentData } from "../types/IPost";
 import { api } from "./api";
 
 const PostList = {
@@ -12,9 +12,29 @@ const PostList = {
       throw error;
     }
   },
-  postData: async(postData: IPost)=>{
+  getPostId: async (id: string) => {
+    try {
+      const response = await api.get(`/posts/${id}`);
+      console.log("Resposta da solicitação:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Erro na solicitação:', error);
+      throw error;
+    }
+  },
+  postData: async (postData: IPost) => {
     try {
       const response = await api.post('/createPost', postData);
+      console.log("Resposta da solicitação:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Erro na solicitação:', error);
+      throw error;
+    }
+  },
+  postReport: async (id: string, postCommentData: IPostCommentData) => {
+    try {
+      const response = await api.post(`/commentPost/${id}`, postCommentData);
       console.log("Resposta da solicitação:", response.data);
       return response.data;
     } catch (error) {

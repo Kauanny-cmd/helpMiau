8ba3eb50f2d1d86c381b5885ba21504d830a3cd7
kauanny-cmd <kauannyvieira8@gmail.com>
2023-12-19 22:31:39 -0300

    #21 add o esboço da tela de filtro

---
+++ mobile/src/screens/Filter/style.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  contSelect: {
+    display: 'flex',
+    flexDirection:'row',
+    gap:8,
+    justifyContent:'space-between',
+  },
+  btts:{
+    display: 'flex',
+    flexDirection:'row',
+  }
+});
+
+export default styles;

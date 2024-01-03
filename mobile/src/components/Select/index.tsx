48766561bfb423cba5970de3bdbfc40e62652051
kauanny-cmd <kauannyvieira8@gmail.com>
2024-01-03 12:59:04 -0300

    #17 check de props vazia

--- mobile/src/components/Select/index.tsx
+++ mobile/src/components/Select/index.tsx
@@ -5,14 +5,21 @@ import style from './style'
 
 interface SelectProps {
   selector: string;
-  selected: boolean;
+  selected?: boolean;
 }
 
 const Select: React.FC<SelectProps> = ({ selector,selected }) =>{
   return(
+    <>
+    {
+      selector == '' ? 
+    <></>
+    :
     <View style={[style.container, selected ? style.selectedContainer : null]}>
       <Text style={[style.txtContainer, selected ? style.selectedTxt : null]}>{selector}</Text>
     </View>
+    }
+    </>
   )
 }
 
--- mobile/src/components/Select/style.tsx
+++ mobile/src/components/Select/style.tsx
@@ -3,14 +3,16 @@ import Colors from '../../global/style'
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: Colors.primaryColor,
+    backgroundColor: Colors.primaryDarkColor,
     borderRadius: 22,
+    width:100
   },
   txtContainer: {
     paddingHorizontal: 14,
     paddingVertical: 6,
     fontSize: 14,
     color: Colors.darkColor,
+    textAlign:'center',
   },
   selectedContainer:{
     backgroundColor: Colors.secondaryColor,

c847879ac0f3c908b3684a411c63eb010fb20807
kauanny-cmd <kauannyvieira8@gmail.com>
2024-01-09 22:17:46 -0300

    #20 update do usuario

--- mobile/src/services/user.ts
+++ mobile/src/services/user.ts
@@ -1,14 +1,24 @@
+import { IUser } from "../types/IUser";
 import { api } from "./api";
 
 const UserSign = {
   postUser: async (nome: string, login: string, senha: string) => {
     try {
-      const response = await api.post('/register',{
+      const response = await api.post('/register', {
         nome,
         login,
         senha
       });
       console.log("Resposta da solicitação:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Erro na solicitação:', error);
+      throw error;
+    }
+  },
+  updateUser: async (userId: string, userData: IUser) => {
+    try {
+      const response = await api.put(`/usuario/${userId}`, userData);
       return response.data;
     } catch (error) {
       console.error('Erro na solicitação:', error);
---
+++ mobile/src/types/IUser.d.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  nome: string,
+  login: string,
+  avatarUrl: string,
+  telefone: string,
+  pets: string[],
+}

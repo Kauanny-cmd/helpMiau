173e29fe8f26674c680f1e000722bb5c1014cde4
kauanny-cmd <kauannyvieira8@gmail.com>
2024-01-27 18:37:32 -0300

    #21 #17 add alertas  e reorganizaao de infos

--- mobile/src/screens/Post/index.tsx
+++ mobile/src/screens/Post/index.tsx
@@ -3,8 +3,10 @@ import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import MapView, { LatLng, Marker } from 'react-native-maps';
 import { ScrollView } from 'react-native-gesture-handler';
+import { ALERT_TYPE, Dialog, AlertNotificationRoot } from 'react-native-alert-notification';
 
 import { IFilters, IPost } from '../../types/IPost';
+import { IUser } from '../../types/IUser';
 import PostList from '../../services/posts';
 
 import Input from '../../components/Input';
@@ -22,14 +24,54 @@ const Post = () => {
   const [descricao, setDescricao] = useState<string>('');
   const [animal, setAnimal] = useState<string>('');
   const [images, setImages] = useState<string[]>([]);
-  const [userID, setUserID] = useState<string>();
+  const [userData, setUserData] = useState<IUser>();
   const [latitude, setLatitude] = useState<number>(0);
   const [longitude, setLongitude] = useState<number>(0);
 
   const [mapModalVisible, setMapModalVisible] = useState(false);
   const [filterModalVisible, setFilterModalVisible] = useState(false);
   const [selectedCoordinates, setSelectedCoordinates] = useState<LatLng | null>(null);
-  const [selectedFilters, setSelectedFilters] = useState<{ [key: string]: string[] }>({});
+  const [selectedFilters, setSelectedFilters] = useState<{ [key: string]: string[] } | null>(null);
+  const [resetImages, setResetImages] = useState(false);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const getDataFromStorage = async (key: string) => {
+          try {
+            const jsonValue = await AsyncStorage.getItem(key);
+            if (jsonValue !== null) {
+              const data = JSON.parse(jsonValue);
+              console.log('Dados recuperados do AsyncStorage:', data);
+              return data;
+            } else {
+              console.log('Nenhum dado encontrado para a chave:', key);
+              return null;
+            }
+          } catch (error) {
+            console.error('Erro ao recuperar dados do AsyncStorage:', error);
+            return null;
+          }
+        };
+
+        const retrievedData = await getDataFromStorage('userData');
+        setUserData(retrievedData)
+
+        if (selectedCoordinates) {
+          setLatitude(selectedCoordinates.latitude);
+          setLongitude(selectedCoordinates.longitude);
+        }
+
+        if (selectedFilters) {
+          setSelectedFilters(null)
+        }
+
+      } catch (error) {
+        console.error('Erro ao recuperar dados do Async', error);
+      }
+    }
+    loadData()
+  }, [selectedCoordinates]);
 
   const openMapModal = () => {
     setMapModalVisible(true);
@@ -48,38 +90,22 @@ const Post = () => {
     console.log(selectedFilters);
   };
 
-
   const handleMarkerClick = (coordinates: LatLng) => {
     setSelectedCoordinates(coordinates);
     openMapModal();
   };
 
-  const handleFilterClick = (filters:IFilters) => {
+  const handleFilterClick = (filters: IFilters) => {
     setSelectedFilters({
-      Cor:filters.Cor,
-      FaseVida: filters.FaseVida,
+      Tipo: filters.Tipo,
       Porte: filters.Porte,
-      Sexo: filters.Sexo,
-      Tipo: filters.Tipo
+      Pelo: filters.Pelo,
+      Cor: filters.Cor,
+      FaseVida: filters.FaseVida,
+      Sexo: filters.Sexo
     });
     openFilterModal();
   };
-
-  useEffect(() => {
-    if (selectedCoordinates) {
-      setLatitude(selectedCoordinates.latitude);
-      setLongitude(selectedCoordinates.longitude);
-    }
-  }, [selectedCoordinates]);
-
-  const setDataAsync = async () => {
-    const dataStorage = await AsyncStorage.getItem('userID')
-    if (dataStorage) {
-      const userData = JSON.parse(dataStorage);
-      setUserID(userData)
-      console.log(userData)
-    }
-  }
 
   const resetFields = () => {
     setDescricao('');
@@ -89,16 +115,36 @@ const Post = () => {
     setLongitude(0);
     setSelectedCoordinates(null);
     setSelectedFilters({})
+
+    setResetImages(true);
+    setTimeout(() => {
+      setResetImages(false);
+    }, 1000);
   };
 
   const handleImageSelected = (imageUri: string) => {
     // Adiciona a URI da imagem ao estado images
-    setImages(prevImages => [...prevImages, imageUri]);
-    console.log(images)
+    if (images.length == 3) {
+      images.pop();
+      setImages(prevImages => [...prevImages, imageUri]);
+    } else {
+      setImages(prevImages => [...prevImages, imageUri]);
+    }
   };
 
   const publicPost = async () => {
-    setDataAsync();
+    if (!animal || !descricao || images.length === 0 || latitude === 0 || longitude === 0 || !selectedFilters) {
+      Dialog.show({
+        type: ALERT_TYPE.WARNING,
+        title: 'Atenção!',
+        textBody: 'Por favor, preencha todos os campos da publicação.',
+        autoClose:2500
+      })
+      return;
+    }
+    const filtrosValues = Object.fromEntries(
+      Object.entries(selectedFilters).map(([key, value]) => [key, value || []])
+    );
     try {
       const data: IPost = {
         nomePet: animal,
@@ -106,72 +152,89 @@ const Post = () => {
         imagens: images,
         isPublic: true,
         localizacao: [`${latitude}`, `${longitude}`],
-        filtros: selectedFilters,
-        userId: userID,
+        filtros: filtrosValues,
+        userId: userData?.id,
       };
-
+      console.log(data);
       const response = await PostList.postData(data);
       console.log('Resposta da solicitação:', response);
+
       resetFields();
+      Dialog.show({
+        type: ALERT_TYPE.SUCCESS,
+        title: 'Sucesso!',
+        textBody: 'Publicação enviada!',
+        autoClose:2000
+      })
     } catch (error) {
+      resetFields();
       console.error('Erro ao publicar:', error);
     }
   };
 
+  const editFiltros = () => {
+    setFilterModalVisible(true);
+  };
+
   return (
     <Container backgroundColor={'#F8F9FA'}>
-      <View>
-        <ScrollView style={style.container}
-          contentContainerStyle={{ rowGap: 12 }}
-          horizontal={false}
-          showsVerticalScrollIndicator={false}
-        >
-          <Text style={style.textTop}>Nova postagem</Text>
-
-          <View style={style.containerView}>
-            <Text style={style.subText}>Fotos</Text>
-            <View style={style.imagesAnimal}>
-              <ImageUpload onImageSelected={handleImageSelected} />
-              <ImageUpload onImageSelected={handleImageSelected} />
-              <ImageUpload onImageSelected={handleImageSelected} />
+       <AlertNotificationRoot>
+      <ScrollView style={style.container}
+        contentContainerStyle={{ rowGap: 12, padding: 4, paddingBottom: 30 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={style.containerView}>
+          <Text style={style.subText}>Fotos</Text>
+          <View style={style.imagesAnimal}>
+            <ImageUpload onImageSelected={handleImageSelected} resetImages={resetImages} />
+            <ImageUpload onImageSelected={handleImageSelected} resetImages={resetImages} />
+            <ImageUpload onImageSelected={handleImageSelected} resetImages={resetImages} />
+          </View>
+        </View>
+        <View style={style.containerView}>
+          <Text style={style.subText}>Nome do animal</Text>
+          <Input
+            placeholder=""
+            value={animal}
+            onChange={(value) => setAnimal(value)}
+          />
+        </View>
+        <View style={style.containerView}>
+          <Text style={style.subText}>Descrição</Text>
+          <Input
+            placeholder=""
+            onChange={(value) => setDescricao(value)}
+            value={descricao}
+          />
+        </View>
+        <View style={style.containerView}>
+          <Text style={style.subText}>Último local visto</Text>
+          {selectedCoordinates ? (
+            <MapView style={style.map} initialRegion={{ ...selectedCoordinates, latitudeDelta: 0.01, longitudeDelta: 0.01 }}>
+              <Marker coordinate={selectedCoordinates} title="Localização selecionada" />
+            </MapView>
+          ) : (
+            <View>
+              <TouchableOpacity style={style.btt} onPress={openMapModal}>
+                <Text style={style.txtBotao}>Selecione a última localização</Text>
+              </TouchableOpacity>
             </View>
-          </View>
-          <View style={style.containerView}>
-            <Text style={style.subText}>Nome do animal</Text>
-            <Input
-              placeholder=""
-              value={animal}
-              onChange={(value) => setAnimal(value)}
-            />
-          </View>
-          <View style={style.containerView}>
-            <Text style={style.subText}>Descrição</Text>
-            <Input
-              placeholder=""
-              onChange={(value) => setDescricao(value)}
-              value={descricao}
-            />
-          </View>
-          <View style={style.containerView}>
-            <Text style={style.subText}>Último local visto</Text>
-            {selectedCoordinates ? (
-              <MapView style={style.map} initialRegion={{ ...selectedCoordinates, latitudeDelta: 0.01, longitudeDelta: 0.01 }}>
-                <Marker coordinate={selectedCoordinates} title="Localização selecionada" />
-              </MapView>
-            ) : (
-              <View>
-                <TouchableOpacity style={style.btt} onPress={openMapModal}>
-                  <Text style={style.txtBotao}>Selecione a última localização</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-          </View>
-
-          <View style={style.containerView}>
+          )}
+        </View>
+        <View style={style.containerView}>
+          <View style={style.viewEdit}>
             <Text style={style.subText}>Filtros</Text>
-            {selectedFilters ? //arrumar visualização
-              (
-                <View style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', width: '100%', justifyContent:"flex-start" }}>
+            {
+              selectedFilters ?
+                <TouchableOpacity onPress={() => editFiltros()}>
+                  <Text style={style.editText}>Editar</Text>
+                </TouchableOpacity>
+                : <></>
+            }
+          </View>
+          {selectedFilters ?
+            (
+              <View style={style.viewFilters}>
                 {Object.keys(selectedFilters).map((category) => (
                   <View key={category}>
                     <FlatList
@@ -179,34 +242,36 @@ const Post = () => {
                       renderItem={({ item }) => (
                         <Select selector={item} />
                       )}
+                      keyExtractor={(item) => item.toString()}
+                      scrollEnabled={false}
                     />
                   </View>
                 ))}
               </View>
-              )
-              :
-              (
-                <View>
-                  <TouchableOpacity style={style.btt} onPress={openFilterModal}>
-                    <Text style={style.txtBotao}>Selecione as palavras chaves</Text>
-                  </TouchableOpacity>
-                </View>
-              )
-            }
-          </View>
-          <View style={style.containerView}>
-            <Button
-              onPress={() => publicPost()}
-              colorBorder={Colors.primaryColor}
-              colorButton={Colors.primaryColor}
-              colorText={Colors.whiteColor}
-              title="Publicar"
-            />
-          </View>
-        </ScrollView>
-      </View>
+            )
+            :
+            (
+              <View>
+                <TouchableOpacity style={style.btt} onPress={openFilterModal}>
+                  <Text style={style.txtBotao}>Selecione as palavras chaves</Text>
+                </TouchableOpacity>
+              </View>
+            )
+          }
+        </View>
+        <View style={style.containerView}>
+          <Button
+            onPress={publicPost}
+            colorBorder={Colors.primaryColor}
+            colorButton={Colors.primaryColor}
+            colorText={Colors.whiteColor}
+            title="Publicar"
+          />
+        </View>
+      </ScrollView>
       <MapModal visible={mapModalVisible} onClose={closeMapModal} onMarkerClick={handleMarkerClick} />
-      <FilterModal visible={filterModalVisible} onClose={closeFilterModal} onFilter={handleFilterClick} />
+      <FilterModal visible={filterModalVisible} onClose={closeFilterModal} onFilter={handleFilterClick} initialSelectedItems={selectedFilters} post={true}/>
+      </AlertNotificationRoot>
     </Container>
   );
 };
--- mobile/src/screens/Post/style.tsx
+++ mobile/src/screens/Post/style.tsx
@@ -1,45 +1,62 @@
 import { StyleSheet } from "react-native";
-import Colors  from "../../global/style"
+import Colors from "../../global/style"
 
 const style = StyleSheet.create({
   container: {
-   padding:4,
-   marginTop:12
+    padding: 0,
   },
-  textTop:{
-    fontSize:20,
-    fontWeight:'500',
+  textTop: {
+    fontSize: 20,
+    fontWeight: '500',
     textAlign: 'center',
   },
-  subText:{
-    fontSize:14,
-    fontWeight:'400',
+  subText: {
+    fontSize: 14,
+    fontWeight: '400',
   },
-  imagesAnimal:{
-    display:'flex',
-    flexDirection:'row',
-    gap:8,
-    justifyContent:'center',
-    padding:4
+  editText: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: Colors.primaryColor
+  },
+  viewEdit: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  imagesAnimal: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 8,
+    justifyContent: 'center',
+    padding: 4
   },
   map: {
     width: '100%',
     height: 130,
-    borderRadius:20
+    borderRadius: 20
   },
-  containerView:{
-    gap:8
+  containerView: {
+    gap: 8
   },
-  btt:{
+  btt: {
     backgroundColor: Colors.primaryLightColor,
-    width:'100%',
-    justifyContent:'center',
-    borderRadius:4,
-    padding:12,
+    width: '100%',
+    justifyContent: 'center',
+    borderRadius: 4,
+    padding: 12,
   },
-  txtBotao:{
-    fontSize:12,
+  txtBotao: {
+    fontSize: 12,
     color: Colors.primaryColor,
+  },
+  viewFilters: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '100%',
+    justifyContent: 'center'
   }
 });
 
